c244a5f2c8d7a48d4eb899c659b5de4e6cdc5872
harisMunandar29 <charismunandar29@gmail.com>
2022-09-17 11:55:23 +0700

    web movie HTML CSS JS

---
+++ main.js
@@ -0,0 +1,30 @@
+const arrow = document.querySelectorAll('.arrow');
+const movieLists = document.querySelectorAll('.movie-list');
+
+arrow.forEach((arrow, i) => {
+  const imgLength = movieLists[i].querySelectorAll('img').length;
+  let clickCounter = 0;
+  arrow.addEventListener('click', () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (imgLength - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get('transform')[0].x.value - 300}px)`;
+    } else {
+      movieLists[i].style.transform = 'translateX(0)';
+      clickCounter = 0;
+    }
+  });
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+// TOGGLE BALL
+
+const ball = document.querySelector('.toggle-ball');
+const items = document.querySelectorAll('.container,.movie-list-title,.nav-container,.sidebar,.left-menu-icon,.toggle');
+
+ball.addEventListener('click', () => {
+  items.forEach((item) => {
+    item.classList.toggle('active');
+  });
+  ball.classList.toggle('active');
+});
